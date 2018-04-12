7e94af1d43326351b9e302ba66dff451cdf7456c
fednem <federico.nemmi@gmail.com>
2018-04-12 17:02:32 +0200

    created all helper functions to create list of submission, search swearword and output ratio of swearwords over number of words

--- count_swear/example_of_use.py
+++ count_swear/example_of_use.py
@@ -5,15 +5,24 @@ Created on Wed Mar 14 16:03:22 2018
 @author: federico nemmi
 """
 
+import os.path
+import praw as pw
+import pickle
+from itertools import chain
+import nltk
+from helper_functions import create_list_of_submission_from_subreddit, from_sub_to_list, count_swear_words_in_text, count_words_in_text, swear_ratio
 
+#get to the right directory
+os.chdir("C:\\Users\\federico nemmi\\Documents\\Python Scripts\\reddit\\count_swear\\count_swear")
+
+#create set of credentials to create a reddit instance
 username="python_project"
 pwd="16RueDuFoyerToulousain"
 client_id="39j6UmIRmny6rA"
 client_secret="4zu-BCjOu4jsITantk95onKNiek"
 user_agent="count_swear by project_python"
 
-import praw as pw
-
+#create the instance
 #open a reddit instance 
 test_reddit = pw.Reddit(client_id=client_id,
                      client_secret=client_secret,
@@ -21,37 +30,40 @@ test_reddit = pw.Reddit(client_id=client_id,
                      username = username,
                      password = pwd)
 
+#create a list of submissions and comments
 the_donald_subs = create_list_of_submission_from_subreddit(test_reddit, "the_donald")
-
 the_donald_list = from_sub_to_list(the_donald_subs)
 
 
 # Saving the list for benchmarking:
-import pickle
 
-with open('the_donald_list_20032018.pkl', 'wb') as f:  
+
+with open('the_donald_list_12042018.pkl', 'wb') as f:  
     pickle.dump([the_donald_list], f)
 
-#check if it works good
-for i,t in enumerate(the_donald_list):
-    print(str(i) + " " + t)    
+#reopen the list RUN FROM HERE FROM THE SECOND TIME
+the_donald_list = pickle.load( open('the_donald_list_20032018.pkl', "rb" ) )
+
+
 
 my_swear_list = ["fuck", "bitch", "hell", "faggot", "cunt", "dick", "prick", "ass", "asshole", "fucking", "crap"]    
-test_list = [the_donald_list[i] for i in (1087,1090,1918,1919)]
-
 match = []
-for text in test_list:
-    local_count = count_swear_words_in_text(text, my_swear_list, error = "{e<=1}")
+for text in the_donald_list:
+    local_count = count_swear_words_in_text(text, my_swear_list, error = "")
     match.append(local_count)
 
-from itertools import chain
+#best results seems to be achieved without fuzzy matching (at least for short word, maybe think of being able to give a list of 
+#fuzzy matching string that match different word)
 
-[element for element in chain(*match) if bool(element)]
+only_matched = [element for element in chain(*match) if bool(element)]
+n_of_match = len(only_matched)
 
-
-total_count = 0
+tot_n = 0
 for text in the_donald_list:
-    local_count = count_swear_words_in_text(text, my_swear_list)
-    total_count = total_count + local_count
-        
+    n = count_words_in_text(text)
+    tot_n += n
 
+swear_ratio = n_of_match/tot_n
+
+
+matching, number_of_swear, ratio = swear_ratio(the_donald_list, swear_words_list = my_swear_list)
--- count_swear/helper_functions.py
+++ count_swear/helper_functions.py
@@ -5,23 +5,9 @@ Created on Wed Mar 14 09:16:27 2018
 @author: federico nemmi
 """
 
-
-username="python_project"
-pwd="16RueDuFoyerToulousain"
-client_id="39j6UmIRmny6rA"
-client_secret="4zu-BCjOu4jsITantk95onKNiek"
-user_agent="count_swear by project_python"
-
-import praw as pw
-
-#open a reddit instance 
-test_reddit = pw.Reddit(client_id=client_id,
-                     client_secret=client_secret,
-                     user_agent=user_agent,
-                     username = username,
-                     password = pwd)
-
-
+import praw
+import regex
+from nltk.corpus import stopwords
 
 #for the moment it only take the top submission
 def create_list_of_submission_from_subreddit(reddit_instance,subreddit):
@@ -40,18 +26,53 @@ def from_sub_to_list(list_of_submission):
     return final_result
 
     
-#here to debug
-import regex
+#give matching as ouotput
 def count_swear_words_in_text(text, swear_words_list, error = "{e<=1}"):
+    non_stopword = [word for word in text.split() if word not in stopwords.words("english") ]
     n_swear = [regex.match("(?:" + swear_word + ")" + error, word.lower()) for swear_word
-         in swear_words_list for word in text.split()]
+         in swear_words_list for word in non_stopword]
     return(n_swear)
 
 
+#give number of swearword as output, the one giving matching has been preferred, I leave this here for future reference
+#def count_swear_words_in_text(text, swear_words_list, error = "{e<=1}"):
+#    non_stopword = [word for word in text.split() if word not in stopwords.words("english") ]
+#    n_swear = sum([bool(regex.match("(?:" + swear_word + ")" + error, word)) for swear_word
+#         in swear_words_list for word in non_stopword])
+#    return(n_swear)
+    
 
-import regex
-def count_swear_words_in_text(text, swear_words_list, error = "{e<=1}"):
-    n_swear = sum([bool(regex.match("(?:" + swear_word + ")" + error, word)) for swear_word
-         in swear_words_list for word in text.split()])
-    return(n_swear)
+def count_words_in_text(text):
+    non_stopword = [word for word in text.split() if word not in stopwords.words("english") ]
+    return len(non_stopword)
+
+def swear_ratio(list_of_post, swear_words_list, error = ""):
+    from itertools import chain
+    match = []
+    for text in list_of_post:
+        local_count = count_swear_words_in_text(text, swear_words_list, error = "")
+        match.append(local_count)
+    only_matched = [element for element in chain(*match) if bool(element)]
+    n_of_match = len(only_matched)
+    
+    tot_n = 0
+    for text in list_of_post:
+        n = count_words_in_text(text)
+        tot_n += n
+        
+    swear_ratio = n_of_match/tot_n
+    
+    return only_matched, n_of_match, swear_ratio
+    
+#subreddit is a list of name, kwargs may be the error in the fuzzy search
+def compare_subreddits(*args):
+    output_dict = {}
+    for subreddit in args:
+        submissions = create_list_of_submission_from_subreddit(reddit_instance, subreddit)
+        submissions_list = from_sub_to_list(submissions)
+        flattened_list = [i for i in chain(*submissions_list)]
+        
+        
+        
+        
     
